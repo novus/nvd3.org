145f40b36c782d96d62a8cfd63b4ee975363f321
Robin Hu <robinfhu@gmail.com>
2013-09-08 12:10:21 -0400

    Added more text to index.html. Updated pie example.

--- ghpages/pie.js
+++ ghpages/pie.js
@@ -1,4 +1,4 @@
-
+//Regular pie chart example
 nv.addGraph(function() {
   var chart = nv.models.pieChart()
       .x(function(d) { return d.label })
@@ -7,35 +7,35 @@ nv.addGraph(function() {
 
     d3.select("#chart svg")
         .datum(exampleData())
-      .transition().duration(1200)
+        .transition().duration(350)
         .call(chart);
 
   return chart;
 });
 
+//Donut chart example
 nv.addGraph(function() {
   var chart = nv.models.pieChart()
       .x(function(d) { return d.label })
       .y(function(d) { return d.value })
-      .showLabels(true)
-      .labelThreshold(.05)
-      .donut(true);
+      .showLabels(true)     //Display pie labels
+      .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+      .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+      .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+      .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+      ;
 
     d3.select("#chart2 svg")
         .datum(exampleData())
-      .transition().duration(1200)
+        .transition().duration(350)
         .call(chart);
 
   return chart;
 });
 
-
-
+//Pie chart example data. Note how there is only a single array of key-value pairs.
 function exampleData() {
-  return [
-  {
-    key: "Cumulative Return",
-    values: [
+  return  [
       { 
         "label": "One",
         "value" : 29.765957771107
@@ -68,9 +68,7 @@ function exampleData() {
         "label": "Eight",
         "value" : 5.1387322875705
       }
-    ]
-  }
-  ];
+    ];
 }
 
 
