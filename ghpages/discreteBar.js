a93c510b42ad380eadfec8e2d7931d416a9a0e92
Robin Hu <robinfhu@gmail.com>
2013-09-07 23:47:10 -0400

    Updated discrete bar.

--- ghpages/discreteBar.js
+++ ghpages/discreteBar.js
@@ -1,17 +1,15 @@
-
-
-
 nv.addGraph(function() {
   var chart = nv.models.discreteBarChart()
-      .x(function(d) { return d.label })
+      .x(function(d) { return d.label })    //Specify the data accessors.
       .y(function(d) { return d.value })
-      .staggerLabels(true)
-      .tooltips(false)
-      .showValues(true)
+      .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
+      .tooltips(false)        //Don't show tooltips
+      .showValues(true)       //...instead, show the bar value right on top of each bar.
+      .transitionDuration(350)
+      ;
 
   d3.select('#chart svg')
       .datum(exampleData())
-    .transition().duration(500)
       .call(chart);
 
   nv.utils.windowResize(chart.update);
@@ -19,47 +17,42 @@ nv.addGraph(function() {
   return chart;
 });
 
-
-
-
-
-
-
+//Each bar represents a single discrete quantity.
 function exampleData() {
  return  [ 
     {
       key: "Cumulative Return",
       values: [
         { 
-          "label" : "A" ,
+          "label" : "A Label" ,
           "value" : -29.765957771107
         } , 
         { 
-          "label" : "B" , 
+          "label" : "B Label" , 
           "value" : 0
         } , 
         { 
-          "label" : "C" , 
+          "label" : "C Label" , 
           "value" : 32.807804682612
         } , 
         { 
-          "label" : "D" , 
+          "label" : "D Label" , 
           "value" : 196.45946739256
         } , 
         { 
-          "label" : "E" ,
+          "label" : "E Label" ,
           "value" : 0.19434030906893
         } , 
         { 
-          "label" : "F" , 
+          "label" : "F Label" , 
           "value" : -98.079782601442
         } , 
         { 
-          "label" : "G" , 
+          "label" : "G Label" , 
           "value" : -13.925743130903
         } , 
         { 
-          "label" : "H" , 
+          "label" : "H Label" , 
           "value" : -5.1387322875705
         }
       ]
