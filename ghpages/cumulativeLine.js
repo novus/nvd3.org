27acead62d3f32ed99c988901d361b820f1b5e37
Robin Hu <robinfhu@gmail.com>
2013-09-08 11:24:22 -0400

    Work in progress on updating the nvd3 examples.

--- ghpages/cumulativeLine.js
+++ ghpages/cumulativeLine.js
@@ -1,12 +1,14 @@
-
 d3.json('cumulativeLineData.json', function(data) {
   nv.addGraph(function() {
     var chart = nv.models.cumulativeLineChart()
                   .x(function(d) { return d[0] })
                   .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
-                  .color(d3.scale.category10().range());
+                  .color(d3.scale.category10().range())
+                  .useInteractiveGuideline(true)
+                  ;
 
      chart.xAxis
+        .tickValues([1078030800000,1122782400000,1167541200000,1251691200000])
         .tickFormat(function(d) {
             return d3.time.format('%x')(new Date(d))
           });
@@ -16,7 +18,6 @@ d3.json('cumulativeLineData.json', function(data) {
 
     d3.select('#chart svg')
         .datum(data)
-      .transition().duration(500)
         .call(chart);
 
     //TODO: Figure out a good way to do this automatically
--- ghpages/multiBarHorizontal.js
+++ ghpages/multiBarHorizontal.js
@@ -1,20 +1,19 @@
-
 d3.json('multiBarHorizontalData.json', function(data) {
   nv.addGraph(function() {
     var chart = nv.models.multiBarHorizontalChart()
         .x(function(d) { return d.label })
         .y(function(d) { return d.value })
         .margin({top: 30, right: 20, bottom: 50, left: 175})
-        .showValues(true)
-        .tooltips(false)
-        .showControls(false);
+        .showValues(true)           //Show bar value next to each bar.
+        .tooltips(true)             //Show tooltips on hover.
+        .transitionDuration(350)
+        .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
 
     chart.yAxis
         .tickFormat(d3.format(',.2f'));
 
     d3.select('#chart1 svg')
         .datum(data)
-      .transition().duration(500)
         .call(chart);
 
     nv.utils.windowResize(chart.update);
@@ -22,4 +21,3 @@ d3.json('multiBarHorizontalData.json', function(data) {
     return chart;
   });
 });
-
